f75ea94d2e0019dfab6df50634d668c09c581d6e
qichang.dong <qichang.dong@videt.cn>
2021-07-23 19:41:30 +0800

    u

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
   python run.py test ./data/test.txt ./result.txt ./vocab/sent_vocab.json ./vocab/tag_vocab.json ./model/model.pth --cuda | tee result_score.txt
-  echo "\nperl conlleval.pl" | tee -a result_score.txt
-  perl ./script/conlleval.pl < result.txt | tee -a result_score.txt
+  # echo "\nperl conlleval.pl" | tee -a result_score.txt
+  # perl ./script/conlleval.pl < result.txt | tee -a result_score.txt
